2a9fd1a82d10bee3f7d81387be111787648edbea
zzdHub <92717210+zzdHub@users.noreply.github.com>
2026-01-25 12:27:42 +0800

    测试本地工程大json 文件上传下载

--- hhh.ps1
+++ hhh.ps1
@@ -1,15 +1,7 @@
-# 要写入的内容（模拟“生成的数据”）
-$data = @{
-    status = 'ok'
-    message = 'excute ok'
-    time = (Get-Date).ToString("yyyy-MM-dd HH:mm:ss")
-}
 
-# 转成 JSON
-$json = $data | ConvertTo-Json -Depth 3
 
 # 输出目录（当前仓库根目录）
-$file = "D:\a\autos\autos\1.json"
+$file = "D:\a\autos\autos\ls\UnFormattedConfig.json"
 
 # 写入文件（不存在会创建，存在会覆盖）
 $json | Set-Content -Path $file -Encoding UTF8
