59e60604d5d611ce6ddb6a4d95c810c02f3ea2d1
zzdHub <92717210+zzdHub@users.noreply.github.com>
2026-01-25 19:04:35 +0800

    Update hhh.ps1

--- hhh.ps1
+++ hhh.ps1
@@ -1,13 +1,5 @@
 # 在 script.ps1 的最开头添加
 # 1. 如果 pwsh 存在，重新用 pwsh 执行当前脚本
-if ($PSVersionTable.PSVersion.Major -lt 7) {
-    if (Get-Command pwsh -ErrorAction SilentlyContinue) {
-        
-        pwsh -NoProfile -ExecutionPolicy Bypass -File $MyInvocation.MyCommand.Path @args
-        Write-Host "检测到 Windows PowerShell，切换到 PowerShell 7..."
-        exit $LASTEXITCODE
-    }
-}
 
 # 2. 继续执行你的代码
 Write-Host "=== 当前 PowerShell 版本 ==="
