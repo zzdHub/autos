c13fa0573456bf61fce9e256678201b74180b376
zzdHub <92717210+zzdHub@users.noreply.github.com>
2026-01-25 19:02:42 +0800

    Update hhh.ps1

--- hhh.ps1
+++ hhh.ps1
@@ -2,8 +2,9 @@
 # 1. 如果 pwsh 存在，重新用 pwsh 执行当前脚本
 if ($PSVersionTable.PSVersion.Major -lt 7) {
     if (Get-Command pwsh -ErrorAction SilentlyContinue) {
-        Write-Host "检测到 Windows PowerShell，切换到 PowerShell 7..."
+        
         pwsh -NoProfile -ExecutionPolicy Bypass -File $MyInvocation.MyCommand.Path @args
+        Write-Host "检测到 Windows PowerShell，切换到 PowerShell 7..."
         exit $LASTEXITCODE
     }
 }
