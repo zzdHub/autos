31e23e5acc7fffe5eaa9b954ace286a428a93639
zzdHub <92717210+zzdHub@users.noreply.github.com>
2026-01-25 11:59:04 +0800

    Update message strings in hhh.ps1

--- hhh.ps1
+++ hhh.ps1
@@ -1,7 +1,7 @@
 # 要写入的内容（模拟“生成的数据”）
 $data = @{
-    status = "ok"
-    message = "执行成功"
+    status = 'ok'
+    message = 'excute ok'
     time = (Get-Date).ToString("yyyy-MM-dd HH:mm:ss")
 }
 
@@ -14,5 +14,5 @@ $file = "D:\a\autos\autos\1.json"
 # 写入文件（不存在会创建，存在会覆盖）
 $json | Set-Content -Path $file -Encoding UTF8
 
-Write-Host "已生成文件 $file"
+Write-Host "its ok: $file"
 
