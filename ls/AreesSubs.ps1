018e77fe72ec5e5458053b8669a191aab64759fd
zzdHub <92717210+zzdHub@users.noreply.github.com>
2026-02-03 22:48:48 +0800

    commit new subs

--- ls/AreesSubs.ps1
+++ ls/AreesSubs.ps1
@@ -1,9 +1,11 @@
 # 定义订阅地址数组
 $subs = @(
     #"https://zhuzhuzhu.whtjdasha.com/api/v1/client/subscribe?token=efa5a5494738b30aec6d9b0045eb12f0",
-    "https://vyy.cqsvhb.cn/s/66f60e99a9aaac93d9ad6822d3e940ff"
+    #"https://vyy.cqsvhb.cn/s/66f60e99a9aaac93d9ad6822d3e940ff"
+    "https://vyy.14kbuy.com/s/66f60e99a9aaac93d9ad6822d3e940ff"
     #"https://zhuzhuzhu.whtjdasha.com/api/v1/client/subscribe?token=efa5a5494738b30aec6d9b0045eb12f0"
 )
+
 
 
 
