a242628e8d5925f4507e67b94739dcfc9ba2cf4b
zzdHub <92717210+zzdHub@users.noreply.github.com>
2026-02-07 11:56:09 +0800

    Update AreesSubs.ps1

--- ls/AreesSubs.ps1
+++ ls/AreesSubs.ps1
@@ -2,10 +2,11 @@
 $subs = @(
     #"https://zhuzhuzhu.whtjdasha.com/api/v1/client/subscribe?token=efa5a5494738b30aec6d9b0045eb12f0",
     #"https://vyy.cqsvhb.cn/s/66f60e99a9aaac93d9ad6822d3e940ff"
-    "https://vyy.14kbuy.com/s/66f60e99a9aaac93d9ad6822d3e940ff",
+   # "https://vyy.14kbuy.com/s/66f60e99a9aaac93d9ad6822d3e940ff",
     "https://dnsdy.zangyun.xyz/ai/m3u8/xppt/sub?token=6b814649ecb7792daa9098bc919148e9"
     #"https://zhuzhuzhu.whtjdasha.com/api/v1/client/subscribe?token=efa5a5494738b30aec6d9b0045eb12f0"
 )
+
 
 
 
