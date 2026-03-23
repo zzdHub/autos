15489744b2ea94e5a8507e3defe277bcbb872d28
zzdHub <92717210+zzdHub@users.noreply.github.com>
2026-03-23 18:57:44 +0800

    Add new subscription URL to the array

--- ls/AreesSubs.ps1
+++ ls/AreesSubs.ps1
@@ -2,7 +2,8 @@
 $subs = @(
     #"https://zhuzhuzhu.whtjdasha.com/api/v1/client/subscribe?token=efa5a5494738b30aec6d9b0045eb12f0",
     #"https://vyy.cqsvhb.cn/s/66f60e99a9aaac93d9ad6822d3e940ff"
-   "https://vyy.14kbuy.com/s/66f60e99a9aaac93d9ad6822d3e940ff"
+   "https://vyy.14kbuy.com/s/66f60e99a9aaac93d9ad6822d3e940ff",
+   "https://scurl.izenny.com/s/409f9e91ac694521c2e1a551bb93913a"
 #"https://api.v1.mk/sub?target=singbox&url=https%3A%2F%2Fyuqidingyue.yuqi.best%2Fyuqibest%2Fa1ff55cb0c4ded715d9d6a8087488e38&insert=false&config=https%3A%2F%2Fraw.githubusercontent.com%2FACL4SSR%2FACL4SSR%2Fmaster%2FClash%2Fconfig%2FACL4SSR_Online_MultiCountry.ini&emoji=true&list=false&xudp=false&udp=false&tfo=false&expand=true&scv=false&fdn=false"
    
    # "https://dnsdy.zangyun.xyz/ai/m3u8/xppt/sub?token=6b814649ecb7792daa9098bc919148e9"
