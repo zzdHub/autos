74a3646aec406c92673280e9a7009bc5f849eb89
zzdHub <92717210+zzdHub@users.noreply.github.com>
2026-04-18 18:35:37 +0800

    1

--- ls/AreesSubs.ps1
+++ ls/AreesSubs.ps1
@@ -2,9 +2,11 @@
 $subs = @(
     #"https://zhuzhuzhu.whtjdasha.com/api/v1/client/subscribe?token=efa5a5494738b30aec6d9b0045eb12f0",
     #"https://vyy.cqsvhb.cn/s/66f60e99a9aaac93d9ad6822d3e940ff"
-   "https://vyy.14kbuy.com/s/66f60e99a9aaac93d9ad6822d3e940ff",
+ #  "https://vyy.14kbuy.com/s/66f60e99a9aaac93d9ad6822d3e940ff",
    #"https://scurl.izenny.com/s/409f9e91ac694521c2e1a551bb93913a",
-"https://scurl.izenny.com/s/0af4ef8fa5c33258f8185c07c9675d2c"
+#"https://scurl.izenny.com/s/0af4ef8fa5c33258f8185c07c9675d2c",
+"http://dnshwhq.shunsll.net/s/9d9df6086d905bf11bf97c8c2b9ebb49",
+"https://vyy.14kbuy.com/s/66f60e99a9aaac93d9ad6822d3e940ff"
    
 #"https://api.v1.mk/sub?target=singbox&url=https%3A%2F%2Fyuqidingyue.yuqi.best%2Fyuqibest%2Fa1ff55cb0c4ded715d9d6a8087488e38&insert=false&config=https%3A%2F%2Fraw.githubusercontent.com%2FACL4SSR%2FACL4SSR%2Fmaster%2FClash%2Fconfig%2FACL4SSR_Online_MultiCountry.ini&emoji=true&list=false&xudp=false&udp=false&tfo=false&expand=true&scv=false&fdn=false"
    
