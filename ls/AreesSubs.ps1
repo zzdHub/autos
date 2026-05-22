f86524969544be9d16a6a4be02d269eaf17e8232
zzdHub <92717210+zzdHub@users.noreply.github.com>
2026-05-22 21:49:27 +0800

    update new-subs

--- ls/AreesSubs.ps1
+++ ls/AreesSubs.ps1
@@ -1,10 +1,8 @@
 # 定义订阅地址数组
 $subs = @(
    
-"https://cszy.yvvy.xyz/s/66f60e99a9aaac93d9ad6822d3e940ff"
-
-   
-
+"https://cszy.yvvy.xyz/s/66f60e99a9aaac93d9ad6822d3e940ff",
+"http://8.134.143.129:5996/s/9b54b030db71c23637064bb5af5a6ce0"
 )
 
 
