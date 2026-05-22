51c35820bb0ca0963721468d77b298118124ad54
zzdHub <92717210+zzdHub@users.noreply.github.com>
2026-05-22 22:53:34 +0800

    Update AreesSubs.ps1

--- ls/AreesSubs.ps1
+++ ls/AreesSubs.ps1
@@ -2,7 +2,9 @@
 $subs = @(
    
 "https://cszy.yvvy.xyz/s/66f60e99a9aaac93d9ad6822d3e940ff",
-"http://8.134.143.129:5996/s/9b54b030db71c23637064bb5af5a6ce0"
+#"http://8.134.143.129:5996/s/9b54b030db71c23637064bb5af5a6ce0"
+"http://8.134.143.129:5996/s/ddf4d9d3b6e2cbc912be18f8289464f9"
+
 )
 
 
