c2f6ab78bb7adf849338080c6190b6a56dc54176
zzdHub <92717210+zzdHub@users.noreply.github.com>
2026-06-09 22:03:21 +0800

    Update AreesSubs.ps1

--- ls/AreesSubs.ps1
+++ ls/AreesSubs.ps1
@@ -1,11 +1,6 @@
 # 定义订阅地址数组
 $subs = @(
-   
- "https://apit.yun7g.top/s/66f60e99a9aaac93d9ad6822d3e940ff",
-
-"http://8.134.143.129:5996/s/a1a33a0b50fa74931c188a8062726a17"
-
-
+   "https://apit.yun7g.top/s/66f60e99a9aaac93d9ad6822d3e940ff"
 )
 
 
