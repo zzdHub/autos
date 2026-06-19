7ea57bd69e3b166336a9414bf137653e2cdb131b
zzdHub <92717210+zzdHub@users.noreply.github.com>
2026-06-19 13:58:36 +0800

    add subs

--- ls/AreesSubs.ps1
+++ ls/AreesSubs.ps1
@@ -1,7 +1,7 @@
 # 定义订阅地址数组
 $subs = @(
-"http://8.134.143.129:5996/s/f09c4138a20720183e895ff3fbe5afab"
-,"http://141.11.148.11:5778/s/b98f673ccec7eba04f0524c2b38e6865"
+"https://api.v1.mk/sub?target=singbox&url=https%3A%2F%2Fapit.yun7g.top%2Fs%2Fb98f673ccec7eba04f0524c2b38e6865&insert=false&config=https%3A%2F%2Fraw.githubusercontent.com%2FACL4SSR%2FACL4SSR%2Fmaster%2FClash%2Fconfig%2FACL4SSR_Online_Full_NoAuto.ini&emoji=true&list=false&xudp=false&udp=false&tfo=false&expand=true&scv=false&fdn=false&diyua=ShadowRocket"
+
 )
 
 
