08fbac97854681f268bd00ff197ae0595895ea06
zzd <1831772351@qq.com>
2026-06-24 21:14:54 +0800

    修改链接

--- ls/AreesSubs.ps1
+++ ls/AreesSubs.ps1
@@ -1,7 +1,8 @@
 # 定义订阅地址数组
 $subs = @(
 #"https://api.v1.mk/sub?target=mixed&url=https%3A%2F%2Fapit.yun7g.top%2Fs%2Fb98f673ccec7eba04f0524c2b38e6865&insert=false&config=https%3A%2F%2Fraw.githubusercontent.com%2FACL4SSR%2FACL4SSR%2Fmaster%2FClash%2Fconfig%2FACL4SSR_Online_Full_NoAuto.ini&emoji=true&list=false&xudp=false&udp=false&tfo=false&expand=true&scv=false&fdn=false&diyua=ShadowRocket"
-"http://8.134.143.129:5996/s/962252a800e592124c27d434d9a1d619"
+# "http://8.134.143.129:5996/s/962252a800e592124c27d434d9a1d619"
+"http://8.134.143.129:5996/s/fa70c58150a5bd41e859048e6e6822c1"
 #"https://api.v1.mk/sub?target=mixed&url=https%3A%2F%2Fapit.yun7g.top%2Fs%2Fb98f673ccec7eba04f0524c2b38e6865&insert=false&config=https%3A%2F%2Fraw.githubusercontent.com%2FACL4SSR%2FACL4SSR%2Fmaster%2FClash%2Fconfig%2FACL4SSR_Online_Full_NoAuto.ini&tls13=true&emoji=true&list=false&xudp=false&udp=false&tfo=false&expand=true&scv=false&fdn=false&diyua=ShadowRocket"
 
 
